5205d1439b060f1eec2d6039fc94a458763fde2c
Matthew Judy <matthew.l.judy@jpmchase.com>
2019-01-17 14:16:03 -0500

    Move proxy switching functions to the standalone functions file.

--- runcoms/rc-functions.zsh
+++ runcoms/rc-functions.zsh
@@ -200,3 +200,42 @@ function brew-installed-options # <package>
 #  alias brew-reinstall-and-add-option 'brew reinstall \!:1 `brew-installed-options \!:1` \!:2*'
 #  set current_options = "`brew-installed-options ffmpeg`" && brew uninstall ffmpeg && brew install ffmpeg ${current_options} '--with-libvpx'
 #
+
+
+################################################################################
+#  CHASE
+
+#
+#  Enable network proxies.
+#
+function proxies-on
+{
+    export HTTP_PROXY="${http_proxy_value}"
+    export HTTPS_PROXY="${https_proxy_value}"
+    export ALL_PROXY="${all_proxy_value}"
+    export NO_PROXY="${no_proxy_value}"
+
+    export http_proxy="${HTTP_PROXY}"
+    export https_proxy="${HTTPS_PROXY}"
+    export all_proxy="${ALL_PROXY}"
+    export no_proxy="${NO_PROXY}"
+}
+
+#
+#  Disable network proxies.
+#
+function proxies-off
+{
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset ALL_PROXY
+    unset NO_PROXY
+
+    unset http_proxy
+    unset https_proxy
+    unset all_proxy
+    unset no_proxy
+}
+#
+
+
