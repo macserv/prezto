ccd439130a0b96b56a28dacd852b75f6f1dca16d
Matthew Judy <matthew.l.judy@jpmchase.com>
2019-01-18 13:14:50 -0500

    Move Chase-specific logic into runcom extension files.

---
+++ runcoms/rc-functions-chase.zsh
@@ -0,0 +1,42 @@
+#
+# Defines Chase-specific user-created functions for shell commands
+#
+
+
+################################################################################
+#  CHASE NETWORK PROXIES
+
+#
+#  Enable network proxies.
+#
+function proxies-on
+{
+    export HTTP_PROXY="${http_proxy_value}"
+    export HTTPS_PROXY="${https_proxy_value}"
+    export ALL_PROXY="${all_proxy_value}"
+    export NO_PROXY="${no_proxy_value}"
+
+    export http_proxy="${HTTP_PROXY}"
+    export https_proxy="${HTTPS_PROXY}"
+    export all_proxy="${ALL_PROXY}"
+    export no_proxy="${NO_PROXY}"
+}
+
+#
+#  Disable network proxies.
+#
+function proxies-off
+{
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset ALL_PROXY
+    unset NO_PROXY
+
+    unset http_proxy
+    unset https_proxy
+    unset all_proxy
+    unset no_proxy
+}
+#
+
+
