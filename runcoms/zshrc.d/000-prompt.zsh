4a7dc70152f61d689d6a6efa8acc5fe362c29b45
Matthew Judy <matt@nibfile.com>
2023-10-19 15:53:41 -0400

    Remove custom macOS proompt modules for Linux config.

--- runcoms/zshrc.d/000-prompt.zsh
+++ runcoms/zshrc.d/000-prompt.zsh
@@ -37,7 +37,7 @@ HISTORY_SUBSTRING_SEARCH_FUZZY='true'
 
 # Configure high-level behaviors
 typeset -g POWERLEVEL9K_MODE='nerdfont-complete'
-typeset -g POWERLEVEL9K_ICON_PADDING='moderate'
+typeset -g POWERLEVEL9K_ICON_PADDING='none'
 typeset -g POWERLEVEL9K_TRANSIENT_PROMPT='always'
 # typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE='true'
 
@@ -55,8 +55,8 @@ typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='%#'
 #
 
 # Lay out segments in four groups: top/bottom line, left/right edge of terminal
-t_l=( 'dir_writable' 'dir' 'vcs' );       t_r=( 'background_jobs' 'my_caffeinate' 'swift_version' 'my_cpu_temp' 'time' )
-b_l=( 'root_indicator' 'os_icon' );                                            b_r=( 'command_execution_time' 'status' )
+t_l=( 'dir_writable' 'dir' 'vcs' );       t_r=( 'background_jobs' 'time' )
+b_l=( 'root_indicator' 'os_icon' );       b_r=( 'command_execution_time' 'status' )
 
 # Combine segment layout into p10k config parameters
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=( ${t_l} 'newline' ${b_l} )
